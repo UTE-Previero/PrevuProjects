cb7dde47fa429239023e9987c5d59facc1d5a60a
Stefano Rivolta <stefano.rivolta@previero.it>
2021-10-07 11:51:58 +0200

    Add test Synoptics project
    to be deleted?   ¯\_(ツ)_/¯

---
+++ Prevu.Synoptics.V1545/SH1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+  /// <summary>
+  /// Interaction logic for SH1.xaml
+  /// </summary>
+  public partial class SH1 : UserControl
+  {
+    public SH1()
+    {
+      InitializeComponent();
+    }
+  }
+}
