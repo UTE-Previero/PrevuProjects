aee5f50d5fa0eab0b59df5a170c091bcb5e15ad5
Stefano Rivolta <stefano.rivolta@previero.it>
2022-02-11 13:08:42 +0100

    Add dashed line controls

---
+++ Prevu.Drawings/Misc/DashedLineVertical.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Misc
+{
+  /// <summary>
+  /// Interaction logic for DashedLineVertical.xaml
+  /// </summary>
+  public partial class DashedLineVertical : UserControl
+  {
+    public DashedLineVertical()
+    {
+      InitializeComponent();
+    }
+  }
+}
