83a08ec49686666d6e8d9bd0786d7e7cfd038890
Stefano Rivolta <stefano.rivolta@previero.it>
2022-08-29 16:28:40 +0200

    Update Drawings

---
+++ Prevu.Drawings/Machines/GR_002.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines
+{
+    /// <summary>
+    /// Interaction logic for GR_001.xaml
+    /// </summary>
+    public partial class GR_002 : UserControl
+  {
+    public GR_002()
+    {
+      InitializeComponent();
+    }
+  }
+}
