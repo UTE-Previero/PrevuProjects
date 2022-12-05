d884d10122c48ec3a1435993e63570d968cac62e
StefanoRivolta-Previero <stefano.rivolta@previero.it>
2022-12-05 11:09:44 +0100

    Add V9997 Test

---
+++ Prevu.Synoptics.V9997-Test/S320.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for GR1.xaml
+    /// </summary>
+    public partial class S320 : UserControl
+    {
+        public S320()
+        {
+            InitializeComponent();
+        }
+
+        private void Pipe_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Manual_Selection_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Radar_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeviceLabel_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Prevu.Synoptics.V9997-Test/S350.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for GR1.xaml
+    /// </summary>
+    public partial class S350 : UserControl
+    {
+        public S350()
+        {
+            InitializeComponent();
+        }
+
+        private void WaterPump_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
