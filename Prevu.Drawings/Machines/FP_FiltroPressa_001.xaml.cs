ef289b2326bb4dae282ff58b5034c07859c90563
Luciano Princi <luciano.princi@3dautomation.it>
2022-12-16 17:11:03 +0100

    Add new machines

---
+++ Prevu.Drawings/Machines/FP_FiltroPressa_001.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines
+{
+    /// <summary>
+    /// Interaction logic for FP_FiltroPressa_001.xaml
+    /// </summary>
+    public partial class FP_FiltroPressa_001 : UserControl
+    {
+        public FP_FiltroPressa_001()
+        {
+            InitializeComponent();
+        }
+    }
+}
