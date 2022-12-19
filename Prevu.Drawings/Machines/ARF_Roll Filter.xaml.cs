8006a6c99aed786a85749279a787be789ff71b5f
Luciano Princi <luciano.princi@3dautomation.it>
2022-12-19 14:51:30 +0100

    Add ARF image

---
+++ Prevu.Drawings/Machines/ARF_Roll Filter.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines
+{
+    /// <summary>
+    /// Interaction logic for ARF_Roll_Filter.xaml
+    /// </summary>
+    public partial class ARF_Roll_Filter : UserControl
+    {
+        public ARF_Roll_Filter()
+        {
+            InitializeComponent();
+        }
+    }
+}
