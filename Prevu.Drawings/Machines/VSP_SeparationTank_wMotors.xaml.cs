32d7c03040a91ca2104da2b846364c0c765510cd
Luciano Princi <luciano.princi@3dautomation.it>
2022-12-23 09:30:38 +0100

    Add component images

---
+++ Prevu.Drawings/Machines/VSP_SeparationTank_wMotors.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines
+{
+    /// <summary>
+    /// Interaction logic for VSP_SeparationTank_wMotors.xaml
+    /// </summary>
+    public partial class VSP_SeparationTank_wMotors : UserControl
+    {
+        public VSP_SeparationTank_wMotors()
+        {
+            InitializeComponent();
+        }
+    }
+}
