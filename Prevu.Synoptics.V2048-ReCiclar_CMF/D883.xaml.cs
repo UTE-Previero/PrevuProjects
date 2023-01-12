35fb544b11b2f79f41e0f71709a0baa3f539fc6e
Luciano Princi <luciano.princi@3dautomation.it>
2023-01-12 16:30:39 +0100

    Update synoptic

---
+++ Prevu.Synoptics.V2048-ReCiclar_CMF/D883.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+  /// <summary>
+  /// Interaction logic for D880.xaml
+  /// </summary>
+  public partial class D883 : UserControl
+  {
+    public D883()
+    {
+      InitializeComponent();
+    }
+
+        private void FKS_FlakesSorter_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SCW_Flex_Verticale_DX_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
