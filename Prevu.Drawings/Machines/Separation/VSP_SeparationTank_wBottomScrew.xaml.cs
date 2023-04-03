06a37a4bf1fb99c6389e7164b3ab5125e5c63414
Gabriele Magni <gabriele.magni@previero.it>
2023-04-03 17:00:10 +0200

    V3168 Sirplaste - update sinottici

---
+++ Prevu.Drawings/Machines/Separation/VSP_SeparationTank_wBottomScrew.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines.Separation
+{
+    /// <summary>
+    /// Logica di interazione per VSP_SeparationTank_wBottomScrew.xaml
+    /// </summary>
+    public partial class VSP_SeparationTank_wBottomScrew : UserControl
+    {
+        public VSP_SeparationTank_wBottomScrew()
+        {
+            InitializeComponent();
+        }
+    }
+}
