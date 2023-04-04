948daab0e498eab036f1a28b4ac1fec74041fd7c
Gabriele Magni <gabriele.magni@previero.it>
2023-04-04 13:11:35 +0200

    V3168 Sirplaste Update Sinottici

---
+++ Prevu.Drawings/Machines/Valves/DV_BigBagDiverter_V2.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines.Valves
+{
+    /// <summary>
+    /// Logica di interazione per DV_BigBagDiverter_V2.xaml
+    /// </summary>
+    public partial class DV_BigBagDiverter_V2 : UserControl
+    {
+        public DV_BigBagDiverter_V2()
+        {
+            InitializeComponent();
+        }
+    }
+}
