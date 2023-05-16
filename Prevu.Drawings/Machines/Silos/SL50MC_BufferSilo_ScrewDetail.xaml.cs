84ae47c70e274768936e8e58c2f56f8e6c485fa4
Gabriele Magni <gabriele.magni@previero.it>
2023-05-16 12:07:02 +0200

    Aggiunte Immagini per macchine V2150

---
+++ Prevu.Drawings/Machines/Silos/SL50MC_BufferSilo_ScrewDetail.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines.Silos
+{
+    /// <summary>
+    /// Logica di interazione per SL50MC_BufferSilo_ScrewDetail.xaml
+    /// </summary>
+    public partial class SL50MC_BufferSilo_ScrewDetail : UserControl
+    {
+        public SL50MC_BufferSilo_ScrewDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
