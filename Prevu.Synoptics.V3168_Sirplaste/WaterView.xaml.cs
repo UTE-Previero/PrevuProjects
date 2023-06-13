0d8179c502efea207eed460244055735fa740564
Gabriele Magni <gabriele.magni@previero.it>
2023-06-13 16:55:30 +0200

    V3168 Inizio aggiornamento Waterview

--- Prevu.Synoptics.V3168_Sirplaste/WaterView.xaml.cs
+++ Prevu.Synoptics.V3168_Sirplaste/WaterView.xaml.cs
@@ -15,14 +15,14 @@ using System.Windows.Shapes;
 
 namespace Prevu.Synoptics
 {
-    /// <summary>
-    /// Interaction logic for PlantView.xaml
-    /// </summary>
-    public partial class WaterView : UserControl
+  /// <summary>
+  /// Interaction logic for PlantView.xaml
+  /// </summary>
+  public partial class WaterView : UserControl
+  {
+    public WaterView()
     {
-        public WaterView()
-        {
-            InitializeComponent();
-        }
+      InitializeComponent();
     }
+  }
 }
