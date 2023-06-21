5b99cdf3e98ab4cfd715d012e7bc05e960a5e667
StefanoRivolta-Previero <stefano.rivolta@previero.it>
2023-06-21 13:23:00 +0200

    Add Water Level to some drawings

--- Prevu.Drawings/Machines/Silos/SLX_Silo.xaml.cs
+++ Prevu.Drawings/Machines/Silos/SLX_Silo.xaml.cs
@@ -1,28 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
+using System.Data;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Prevu.UIComponents.Labels;
 
-namespace Prevu.Drawings.Machines
+namespace Prevu.Drawings.Machines;
+
+/// <summary>
+/// Interaction logic for SLX_Silo.xaml
+/// </summary>
+public partial class SLX_Silo : UserControl
 {
-    /// <summary>
-    /// Interaction logic for SLX_Silo.xaml
-    /// </summary>
-    public partial class SLX_Silo : UserControl
-    {
-        public SLX_Silo()
-        {
-            InitializeComponent();
-        }
-    }
+
+  public static readonly DependencyProperty ComponentNameProperty =
+DependencyProperty.Register(nameof(ComponentName), typeof(string), typeof(SLX_Silo),
+  new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, ComponentNameChangedCallback));
+
+  private static void ComponentNameChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+    => ((SLX_Silo)dependencyObject).Update();
+
+  [Description("Name of the water level component"), Category("_PV")]
+  public string ComponentName
+  {
+    get { return (string)GetValue(ComponentNameProperty); }
+    set { SetValue(ComponentNameProperty, value); }
+  }
+
+  public static readonly DependencyProperty TrendNameProperty =
+    DependencyProperty.Register(nameof(TrendName), typeof(string), typeof(SLX_Silo),
+      new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, TrendNamePropertyChangedCallback));
+
+  private static void TrendNamePropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+    => ((SLX_Silo)dependencyObject).Update();
+
+  [Description("Name of the level trend, percentage only"), Category("_PV")]
+  public string TrendName
+  {
+    get { return (string)GetValue(TrendNameProperty); }
+    set { SetValue(TrendNameProperty, value); }
+  }
+
+  public SLX_Silo()
+  {
+    InitializeComponent();
+  }
+
+  private void Update()
+  {
+    WaterLevel.ComponentName = ComponentName;
+    WaterLevel.TrendName = TrendName;
+  }
 }
--- Prevu.Drawings/Machines/Stirrer/STR_BatchReactor.xaml.cs
+++ Prevu.Drawings/Machines/Stirrer/STR_BatchReactor.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,9 +19,44 @@ namespace Prevu.Drawings.Machines
   /// </summary>
   public partial class STR_BatchReactor : UserControl
   {
+
+    public static readonly DependencyProperty ComponentNameProperty =
+      DependencyProperty.Register(nameof(ComponentName), typeof(string), typeof(STR_BatchReactor),
+new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, ComponentNameChangedCallback));
+
+    private static void ComponentNameChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+      => ((STR_BatchReactor)dependencyObject).Update();
+
+    [Description("Name of the water level component"), Category("_PV")]
+    public string ComponentName
+    {
+      get { return (string)GetValue(ComponentNameProperty); }
+      set { SetValue(ComponentNameProperty, value); }
+    }
+
+    public static readonly DependencyProperty TrendNameProperty =
+      DependencyProperty.Register(nameof(TrendName), typeof(string), typeof(STR_BatchReactor),
+        new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, TrendNamePropertyChangedCallback));
+
+    private static void TrendNamePropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+      => ((STR_BatchReactor)dependencyObject).Update();
+
+    [Description("Name of the level trend, percentage only"), Category("_PV")]
+    public string TrendName
+    {
+      get { return (string)GetValue(TrendNameProperty); }
+      set { SetValue(TrendNameProperty, value); }
+    }
+
     public STR_BatchReactor()
     {
       InitializeComponent();
+    }
+
+    private void Update()
+    {
+      WaterLevel.ComponentName = ComponentName;
+      WaterLevel.TrendName = TrendName;
     }
   }
 }
