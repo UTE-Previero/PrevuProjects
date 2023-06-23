36c00980a3d189a9cfd7f6aabd23d5784c1533b2
StefanoRivolta-Previero <stefano.rivolta@previero.it>
2023-06-23 09:24:00 +0200

    Drawings: Add WaterLevel to Drawings

--- Prevu.Drawings/Machines/Stirrer/STR_PrewashReactor_wScrew.xaml.cs
+++ Prevu.Drawings/Machines/Stirrer/STR_PrewashReactor_wScrew.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,49 @@ using System.Windows.Shapes;
 
 namespace Prevu.Drawings.Machines.Stirrer
 {
-    /// <summary>
-    /// Logica di interazione per STR_PrewashReactor_wScrew.xaml
-    /// </summary>
-    public partial class STR_PrewashReactor_wScrew : UserControl
+  /// <summary>
+  /// Logica di interazione per STR_PrewashReactor_wScrew.xaml
+  /// </summary>
+  public partial class STR_PrewashReactor_wScrew : UserControl
+  {
+
+    public static readonly DependencyProperty ComponentNameProperty =
+  DependencyProperty.Register(nameof(ComponentName), typeof(string), typeof(STR_PrewashReactor_wScrew),
+new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, ComponentNameChangedCallback));
+
+    private static void ComponentNameChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+      => ((STR_PrewashReactor_wScrew)dependencyObject).Update();
+
+    [Description("Name of the water level component"), Category("_PV")]
+    public string ComponentName
     {
-        public STR_PrewashReactor_wScrew()
-        {
-            InitializeComponent();
-        }
+      get { return (string)GetValue(ComponentNameProperty); }
+      set { SetValue(ComponentNameProperty, value); }
     }
+
+    public static readonly DependencyProperty TrendNameProperty =
+      DependencyProperty.Register(nameof(TrendName), typeof(string), typeof(STR_PrewashReactor_wScrew),
+        new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, TrendNamePropertyChangedCallback));
+
+    private static void TrendNamePropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+      => ((STR_PrewashReactor_wScrew)dependencyObject).Update();
+
+    [Description("Name of the level trend, percentage only"), Category("_PV")]
+    public string TrendName
+    {
+      get { return (string)GetValue(TrendNameProperty); }
+      set { SetValue(TrendNameProperty, value); }
+    }
+
+    public STR_PrewashReactor_wScrew()
+    {
+      InitializeComponent();
+    }
+
+    private void Update()
+    {
+      WaterLevel.ComponentName = ComponentName;
+      WaterLevel.TrendName = TrendName;
+    }
+  }
 }
--- Prevu.Drawings/Machines/Stirrer/STR_Washing_reactor_A.xaml.cs
+++ Prevu.Drawings/Machines/Stirrer/STR_Washing_reactor_A.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +16,46 @@ using System.Windows.Shapes;
 
 namespace Prevu.Drawings.Machines
 {
-    /// <summary>
-    /// Interaction logic for STR_Washing_reactor_A.xaml
-    /// </summary>
-    public partial class STR_Washing_reactor_A : UserControl
+  /// <summary>
+  /// Interaction logic for STR_Washing_reactor_A.xaml
+  /// </summary>
+  public partial class STR_Washing_reactor_A : UserControl
+  {
+    public static readonly DependencyProperty ComponentNameProperty =
+      DependencyProperty.Register(nameof(ComponentName), typeof(string), typeof(STR_Washing_reactor_A),
+new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, ComponentNameChangedCallback));
+
+    private static void ComponentNameChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+      => ((STR_Washing_reactor_A)dependencyObject).Update();
+
+    [Description("Name of the water level component"), Category("_PV")]
+    public string ComponentName
     {
-        public STR_Washing_reactor_A()
-        {
-            InitializeComponent();
-        }
+      get { return (string)GetValue(ComponentNameProperty); }
+      set { SetValue(ComponentNameProperty, value); }
     }
+
+    public static readonly DependencyProperty TrendNameProperty =
+      DependencyProperty.Register(nameof(TrendName), typeof(string), typeof(STR_Washing_reactor_A),
+        new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, TrendNamePropertyChangedCallback));
+
+    private static void TrendNamePropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+      => ((STR_Washing_reactor_A)dependencyObject).Update();
+
+    [Description("Name of the level trend, percentage only"), Category("_PV")]
+    public string TrendName
+    {
+      get { return (string)GetValue(TrendNameProperty); }
+      set { SetValue(TrendNameProperty, value); }
+    }
+    public STR_Washing_reactor_A()
+    {
+      InitializeComponent();
+    }
+    private void Update()
+    {
+      WaterLevel.ComponentName = ComponentName;
+      WaterLevel.TrendName = TrendName;
+    }
+  }
 }
