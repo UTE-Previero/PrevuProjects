d916f84c02535b4dfc4ff45ae63d8d5318730f6a
Gabriele Magni <gabriele.magni@previero.it>
2023-12-05 13:07:12 +0100

    Prevu.Details Project Fixed

---
+++ Prevu.Details/Hoppers/DET_029_LEV_2Chamber_4Lev_3Lev_FKS.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+  /// <summary>
+  /// Interaction logic for D880.xaml
+  /// </summary>
+  public partial class DET_029_LEV_2Chamber_4Lev_3Lev_FKS : UserControl
+  {
+    public DET_029_LEV_2Chamber_4Lev_3Lev_FKS()
+    {
+      InitializeComponent();
+    }
+
+       
+    }
+}
---
+++ Prevu.Details/Hoppers/DET_xxx_LEV_2Chamber_3Lev_2Lev_FKS.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+  /// <summary>
+  /// Interaction logic for D880.xaml
+  /// </summary>
+  public partial class DET_xxx_LEV_2Chamber_3Lev_2Lev_FKS : UserControl
+  {
+    public DET_xxx_LEV_2Chamber_3Lev_2Lev_FKS()
+    {
+      InitializeComponent();
+    }
+
+       
+    }
+}
