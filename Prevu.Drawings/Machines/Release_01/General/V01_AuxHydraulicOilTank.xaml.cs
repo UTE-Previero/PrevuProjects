b2af9f912b5b2bed53d3d869116d8342cf02b2ad
Gabriele Magni <gabriele.magni@previero.it>
2023-12-18 16:55:36 +0100

    Prevu.Drawings: Fixed some Shadows

---
+++ Prevu.Drawings/Machines/Release_01/General/V01_AuxHydraulicOilTank.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using Prevu.UIComponents.Stores;
+
+namespace Prevu.Drawings.Machines
+{
+  /// <summary>
+  /// Interaction logic for VRC_WaterCollectingTank.xaml
+  /// </summary>
+  public partial class V01_AuxHydraulicOilTank : UserControl
+  {
+    
+    public V01_AuxHydraulicOilTank()
+    {
+      InitializeComponent();
+    }
+
+  }
+}
