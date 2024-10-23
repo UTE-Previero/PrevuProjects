533fb470e360b6340835bd878ddcf2a8e5d43ffc
Gabriele Magni <gabriele.magni@previero.it>
2024-10-23 15:30:08 +0200

    V4066: Added New Project

---
+++ Prevu.Synoptics.V4066_BRR/Pages/P890.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for WH2.xaml
+    /// </summary>
+    public partial class P890 : UserControl
+    {
+        public P890()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
