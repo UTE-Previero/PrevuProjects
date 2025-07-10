770fecd649a5064deb47cf403cb1a6545f9c140f
Gabriele Magni <gabriele.magni@previero.it>
2025-07-10 14:28:07 +0200

    Prevu.Drawing: Added Level Indication on Grinder Image. Used for Calculated filling Current

--- Prevu.Drawings/Machines/Release_01/Grinders/V01_MU_Grinder.xaml.cs
+++ Prevu.Drawings/Machines/Release_01/Grinders/V01_MU_Grinder.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,36 @@ namespace Prevu.Drawings.Machines
     /// </summary>
     public partial class V01_MU_Grinder : UserControl
     {
+        public static readonly DependencyProperty ComponentNameProperty = DependencyProperty.Register(nameof(ComponentName), typeof(string), typeof(V01_MU_Grinder),
+            new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, ComponentNameChangedCallback));
+
+        private static void ComponentNameChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+          => ((V01_MU_Grinder)dependencyObject).Update();
+
+        [Description("Name of the material level component"), Category("_PV")]
+        public string ComponentName
+        {
+            get { return (string)GetValue(ComponentNameProperty); }
+            set { SetValue(ComponentNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty TrendNameProperty = DependencyProperty.Register(nameof(TrendName), typeof(string), typeof(V01_MU_Grinder),
+            new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.AffectsRender, TrendNamePropertyChangedCallback));
+
+        private static void TrendNamePropertyChangedCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+          => ((V01_MU_Grinder)dependencyObject).Update();
+
+        [Description("Name of the level trend, percentage only"), Category("_PV")]
+        public string TrendName
+        {
+            get { return (string)GetValue(TrendNameProperty); }
+            set { SetValue(TrendNameProperty, value); }
+        }
+        private void Update()
+        {
+            MaterialLevel.ComponentName = ComponentName;
+            MaterialLevel.TrendName = TrendName;
+        }
         public V01_MU_Grinder()
         {
             InitializeComponent();
