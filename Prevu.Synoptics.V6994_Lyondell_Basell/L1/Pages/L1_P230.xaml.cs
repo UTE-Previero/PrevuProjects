2af7e8e7799dc0fd7464bb5994d5f1ffc4031836
Gabriele Magni <gabriele.magni@previero.it>
2025-11-12 16:59:01 +0100

    V6994: Lyondell Basell New Project

---
+++ Prevu.Synoptics.V6994_Lyondell_Basell/L1/Pages/L1_P230.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for GR1.xaml
+    /// </summary>
+    public partial class L1_P230 : UserControl
+    {
+        public L1_P230()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Prevu.Synoptics.V6994_Lyondell_Basell/L2/Pages/L2_P350.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for GR1.xaml
+    /// </summary>
+    public partial class L2_P350 : UserControl
+    {
+        public L2_P350()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
