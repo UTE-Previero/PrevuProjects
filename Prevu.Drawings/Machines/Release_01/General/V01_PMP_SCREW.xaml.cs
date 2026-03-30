288ab117ac95f07d6309990b81fbb28a3bc67493
Gabriele Magni <gabriele.magni@previero.it>
2026-03-30 16:06:24 +0200

    Prevu.Drawing: Added Screw Pump

---
+++ Prevu.Drawings/Machines/Release_01/General/V01_PMP_SCREW.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Drawings.Machines
+{
+    /// <summary>
+    /// Logica di interazione per V01_PMP_SCREW.xaml
+    /// </summary>
+    public partial class V01_PMP_SCREW : UserControl
+    {
+        public V01_PMP_SCREW()
+        {
+            InitializeComponent();
+        }
+    }
+}
