fa25be74db7bd93a22baab0aa942f664b03ab31a
Gabriele Magni <gabriele.magni@previero.it>
2026-04-08 08:24:43 +0200

    V7768: Completed washing line area

---
+++ Prevu.Synoptics.V7768_Aliplast-NO/L1/Pages/L1_P580.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for WH1.xaml
+    /// </summary>
+    public partial class L1_P580 : UserControl
+    {
+        public L1_P580()
+        {
+            InitializeComponent();
+
+        }
+
+ 
+    }
+}
