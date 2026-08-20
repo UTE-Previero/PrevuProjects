736d765bad12d5e43a748beba15e52334049d415
Andrea Coira <Andrea.Coira@Previero.it>
2026-08-20 09:39:17 +0200

    V6994: Added L4

---
+++ Prevu.Synoptics.V6994_Lyondell_Basell/L4/Pages/L4_P916.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for GR1.xaml
+    /// </summary>
+    public partial class L4_P916 : UserControl
+    {
+        public L4_P916()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Prevu.Synoptics.V6994_Lyondell_Basell/L4/Pages/L4_P955.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prevu.Synoptics
+{
+    /// <summary>
+    /// Interaction logic for GR1.xaml
+    /// </summary>
+    public partial class L4_P955 : UserControl
+    {
+        public L4_P955()
+        {
+            InitializeComponent();
+        }
+
+        private void Pipe_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Pipe_Loaded_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
